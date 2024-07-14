7889dd25978ebdd9051a24c17869c7cdc015ce6f
Siraj shaik <131347877+rockstarsiraj@users.noreply.github.com>
2024-07-14 12:37:26 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,10 @@
+class data{
+    int a=3;
+    int result=a;
+    float f=3.6f;
+    double d=9.4;
+    char c='3';
+    long l=34l;
+   
+
+}
---
+++ operators.java
@@ -0,0 +1,9 @@
+ class operators {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        int result=a*b;
+        System.out.println(result);
+    }
+    
+}
