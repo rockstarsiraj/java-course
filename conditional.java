72d7f7ade61e796969eb67dd681d056a83bc0d49
Siraj shaik <131347877+rockstarsiraj@users.noreply.github.com>
2024-07-14 13:01:58 +0530

    Add files via upload

---
+++ conditional.java
@@ -0,0 +1,14 @@
+public class conditional {
+    public static void main(String[] args) {
+        int a=9;
+        int b=6;
+
+        if(a>b)
+        System.out.println("a is big");
+        else
+        System.out.println("a is not big");
+    }
+
+    }
+   
+
---
+++ relationaloperators.java
@@ -0,0 +1,10 @@
+class relational{
+    public static void main(String[] args) {
+    
+    int a=8;
+    float f=4.8f;
+    int b=(int)f;
+    boolean result=a>b && b<a;
+    System.out.println(result);
+    }
+}
